6713b28d9d9c4a5dcd0ad6084c55cc51ae1d1c6e
DavidGreyIreland <b00074647@student.itb.ie>
2016-04-04 23:36:41 +0100

    changing UserController to LoginController

---
+++ src/LoginController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Itb;
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+class LoginController
+{
+    // action for POST route:    /processLogin
+    public function processLoginAction(Request $request, Application $app)
+    {
+        // default is bad login
+        $isLoggedIn = false;
+
+        $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+
+        // search for user with username in repository
+        $isLoggedIn = User::canFindMatchingUsernameAndPassword($username, $password);
+
+        // action depending on login success
+        if($isLoggedIn)
+        {
+            // store username in 'user' in 'session'
+            $app['session']->set('user', array('username' => $username) );
+            // success - redirect to the secure admin home page
+            return $app->redirect('/admin');
+        }
+
+        // login page with error message
+        // ------------
+        $templateName = 'login';
+        $argsArray = array(
+            'errorMessage' => 'bad username or password - please re-enter'
+        );
+
+        return $app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
+
+    // action for route:    /login
+    public function loginAction(Request $request, Application $app)
+    {
+        // logout any existing user
+        $app['session']->set('user', null );
+
+        // build args array
+        // ------------
+        $argsArray = [];
+
+        // render (draw) template
+        // ------------
+        $templateName = 'login';
+        return $app['twig']->render($templateName . '.html.twig', $argsArray);
+    }
+
+    // action for route:    /logout
+    public function logoutAction(Request $request, Application $app)
+    {
+        // logout any existing user
+        $app['session']->set('user', null );
+
+        // redirect to home page
+//        return $app->redirect('/');
+
+        // render (draw) template
+        // ------------
+        $templateName = 'index';
+        return $app['twig']->render($templateName . '.html.twig', []);
+    }
+
+    //--------- helper functions -------
+    public function isLoggedInFromSession()
+    {
+        $isLoggedIn = false;
+
+        // user is logged in if there is a 'user' entry in the SESSION superglobal
+        if(isset($_SESSION['user'])){
+            $isLoggedIn = true;
+        }
+
+        return $isLoggedIn;
+    }
+
+    public function usernameFromSession()
+    {
+        $username = '';
+
+        // extract username from SESSION superglobal
+        if (isset($_SESSION['user'])) {
+            $username = $_SESSION['user'];
+        }
+
+        return $username;
+    }
+}
